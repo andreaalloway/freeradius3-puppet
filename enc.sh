2adf8e55828780081be326873518ac1f330c5104
AndrewAlloway <andrew@andrewalloway.com>
2017-09-21 11:19:36 -0600

    Added ENC

--- enc.sh
+++ enc.sh
@@ -4,9 +4,9 @@ node_def="/etc/puppetlabs/code/environments/freeradius3-puppet/clients/${1}.yaml
 default_node="/etc/puppetlabs/code/environments/freeradius3-puppet/clients/default.yaml"
 default_radius="/etc/puppetlabs/code/environments/freeradius3-puppet/clients/default-radius.yaml"
 
-nodename = "${1}"
+node_name="${1}"
 
-if [[ ${nodename,,} == *"radius"* ]]; then
+if [[ ${node_name,,} == *"radius"* ]]; then
 
 [ -f $node_def ] && cat $node_def
 
